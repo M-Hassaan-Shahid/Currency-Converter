dd5b42b0e6073493335347c67a6743a706958a99
Muhammad Hassaan Shahid <2023cs79@student.uet.edu.pk>
2025-07-09 17:09:32 +0500

    c

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const BASE_URL ='https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/btc.min.json';
-const dropdowns =document.querySelectorAll('.dropdown select');
+const BASE_URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/btc.min.json';
+const dropdowns = document.querySelectorAll('.dropdown select');
 
 
-for(let select of dropdowns){
-    for(let currCode in countryList){
-        let option = document.createElement('option');
-        option.value = currCode;
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let option = document.createElement("option");
         option.innerText = currCode;
+        option.value = currCode;
+        if(select.id === "fromCurrency" && currCode === "USD") {
+            option.selected = true;
+        }
+        if(select.id === "toCurrency" && currCode === "EUR") {
+            option.selected = true;
+        }
         select.append(option);
     }
 }
