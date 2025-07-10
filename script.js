18aec171984c60b93254732be422268500f4d1d6
Muhammad Hassaan Shahid <2023cs79@student.uet.edu.pk>
2025-07-10 17:02:11 +0500

    done

--- script.js
+++ script.js
@@ -2,6 +2,39 @@ const API_KEY = '7093f7dc6628eb1b00bd7b2f';
 const dropdowns = document.querySelectorAll('.dropdown select');
 const button = document.getElementById('btn');
 
+const updateValue = async (evt) => {
+    evt.preventDefault();
+    let fromCurrency = document.getElementById("fromCurrency").value;
+    let toCurrency = document.getElementById("toCurrency").value;
+    let amount = document.querySelector(".amount input");
+    let amtValue = amount.value;
+    if (amtValue === "" || isNaN(amtValue) || amtValue <= 0) {
+        alert("Please enter a valid amount");
+        return;
+    }
+    const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${fromCurrency}`;
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        if (data.result !== "success") {
+            throw new Error("API error");
+        }
+
+        const rate = data.conversion_rates[toCurrency];
+        const convertedAmount = (amtValue * rate).toFixed(2);
+
+        document.querySelector(".msg p").innerText =
+            `${amtValue} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+
+    } catch (error) {
+        console.error("Conversion error:", error);
+        alert("Failed to get exchange rate. Try again.");
+    }
+
+}
+
 for (let select of dropdowns) {
     for (let currCode in countryList) {
         let option = document.createElement("option");
@@ -27,34 +60,9 @@ const updateFlag = (element) => {
     img.src = newSrc;
 
 };
-btn.addEventListener("click", async (evt) => {
-    evt.preventDefault();
-    let fromCurrency = document.getElementById("fromCurrency").value;
-    let toCurrency = document.getElementById("toCurrency").value;
-    let amount = document.querySelector(".amount input");
-    let amtValue = amount.value;
-    if (amtValue === "" || isNaN(amtValue) || amtValue <= 1) {
-        alert("Please enter a valid amount");
-        return;
-    }
-    const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${fromCurrency}`;
-
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-
-        if (data.result !== "success") {
-            throw new Error("API error");
-        }
-
-        const rate = data.conversion_rates[toCurrency];
-        const convertedAmount = (amtValue * rate).toFixed(2);
-
-        document.querySelector(".msg p").innerText =
-            `${amtValue} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
-    } catch (error) {
-        console.error("Conversion error:", error);
-        alert("Failed to get exchange rate. Try again.");
-    }
-
+window.addEventListener("load", (evt) => {
+    updateValue(evt);
+})
+btn.addEventListener("click", (evt) => {
+    updateValue(evt);
 });
